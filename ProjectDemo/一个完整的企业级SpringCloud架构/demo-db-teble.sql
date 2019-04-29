c0687e2e19216e9c1ba8161243c67c0428cfed88
liujun19921020 <lj92mi@sina.cn>
2019-04-29 10:36:41 +0800

    项目用到myBatis对应mysql脚本

---
+++ ProjectDemo/一个完整的企业级SpringCloud架构/demo-db-teble.sql
@@ -0,0 +1,39 @@
+CREATE database if NOT EXISTS `cs1_db` default character set utf8 collate utf8_general_ci;
+use `cs1_db`;
+
+CREATE TABLE `ORDER_AMAZON_DETAIL` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `order_id` varchar(50) NOT NULL COMMENT '系统订单号',
+  `sku` varchar(100) NOT NULL COMMENT 'SKU',
+  `site` varchar(20) NOT NULL COMMENT '购买站:美国站,德国站等',
+  `order_status` varchar(4) NOT NULL COMMENT '订单状态',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='订单详情表';
+
+
+INSERT INTO `ORDER_AMAZON_DETAIL`(`id`, `order_id`, `sku`, `site`, `order_status`) VALUES (1, 'AM181213014486', 'JM00056', '美国站', '2');
+INSERT INTO `ORDER_AMAZON_DETAIL`(`id`, `order_id`, `sku`, `site`, `order_status`) VALUES (2, 'AM171229006868', 'JM00012', '德国站', '2');
+INSERT INTO `ORDER_AMAZON_DETAIL`(`id`, `order_id`, `sku`, `site`, `order_status`) VALUES (3, 'AM171229006868', 'JM00012', '德国站', '3');
+INSERT INTO `ORDER_AMAZON_DETAIL`(`id`, `order_id`, `sku`, `site`, `order_status`) VALUES (4, 'AM171229006868', 'JM00012', '德国站', '3');
+INSERT INTO `ORDER_AMAZON_DETAIL`(`id`, `order_id`, `sku`, `site`, `order_status`) VALUES (5, 'AM180222004731', 'JM00038', '美国站', '2');
+
+commit;
+
+
+CREATE database if NOT EXISTS `cs2_db` default character set utf8 collate utf8_general_ci;
+use `cs2_db`;
+
+CREATE TABLE `LJ_PRODUCT` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `sku` varchar(130) DEFAULT NULL,
+  `product_name` varchar(100) NOT NULL COMMENT '名称',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='产品表';
+
+
+INSERT INTO `LJ_PRODUCT`(`id`, `sku`, `product_name`) VALUES (1, 'JM00012', 'Desktop microfono a condensatore');
+INSERT INTO `LJ_PRODUCT`(`id`, `sku`, `product_name`) VALUES (2, 'JM00038', 'Lanterna LED Portatile');
+INSERT INTO `LJ_PRODUCT`(`id`, `sku`, `product_name`) VALUES (3, 'JM00056', 'ZJchao 3V 1A AC Adapter to DC Power Adapter 5.5/2.1mm');
+
+commit;
+
